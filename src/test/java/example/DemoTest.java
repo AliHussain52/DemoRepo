75550e73072df664cd2718180252c28a19c2b4fa
AliHussain52 <motiwala.alihussain@yahoo.com>
2019-03-02 14:49:41 +0530

    First code into GIT

---
+++ src/test/java/example/DemoTest.java
@@ -0,0 +1,21 @@
+package example;
+
+import org.testng.annotations.Test;
+
+public class DemoTest {
+
+	@Test
+	public void methodA() {
+		System.out.println("Inside Method A");
+	}
+
+	@Test
+	public void methodB() {
+		System.out.println("Inside Method B");
+	}
+
+	@Test
+	public void methodC() {
+		System.out.println("Inside Method C");
+	}
+}
---
+++ src/test/java/example/DemoTest2.java
@@ -0,0 +1,21 @@
+package example;
+
+import org.testng.annotations.Test;
+
+public class DemoTest2 {
+
+	@Test
+	public void methodA2() {
+		System.out.println("Inside Method A");
+	}
+
+	@Test
+	public void methodB2() {
+		System.out.println("Inside Method B");
+	}
+
+	@Test
+	public void methodC2() {
+		System.out.println("Inside Method C");
+	}
+}
