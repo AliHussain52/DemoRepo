8d0971f2b86999b6b6c5c6f830651a5784a7c6be
AliHussain52 <motiwala.alihussain@yahoo.com>
2019-03-02 15:18:54 +0530

    Added DemoTest3 class

---
+++ src/test/java/example/DemoTest3.java
@@ -0,0 +1,21 @@
+package example;
+
+import org.testng.annotations.Test;
+
+public class DemoTest3 {
+
+	@Test
+	public void methodA3() {
+		System.out.println("Inside Method A");
+	}
+
+	@Test
+	public void methodB3() {
+		System.out.println("Inside Method B");
+	}
+
+	@Test
+	public void methodC3() {
+		System.out.println("Inside Method C");
+	}
+}
